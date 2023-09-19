b2e2171c457ea31ed48a74f553d76fd8c2158536
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-09-19 10:46:49 -0600

    made gameBoard and Player objects

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let gameBoard = [
-    ['✕', '', ''],
-    ['', 'O', 'O'],
-    ['', '', '✕']
-];
-
 const gameSquares = document.querySelectorAll(".game-square");
 
-function displayBoard() {
-    let i = 0;
+const gameBoard = (() => {
+    const _currentBoard = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 
-    [...gameSquares].forEach((square) => {
-        square.textContent = gameBoard[Math.floor(i / 3)][i % 3];
-        i++;
-    });
-}
+    const displayBoard = (p1Piece, p2Piece) => {
+        let i = 0;
+        let currentPiece = 0;
 
-displayBoard();
+        [...gameSquares].forEach((square) => {
+            currentPiece = _currentBoard[Math.floor(i / 3)][i % 3];
+            square.textContent = currentPiece===1 ? p1Piece : currentPiece===2 ? p2Piece : "";
+            i++;
+        });
+    }
+    return {displayBoard};
+})();
+
+const Player = (name, gamePiece) => {
+    let score = 0;
+
+    return {score, name, gamePiece};
+};
+
+const nemina = Player("Nemina", "X");
+const devina = Player("Devina", "O");
+
+gameBoard.displayBoard(nemina.gamePiece, devina.gamePiece);
